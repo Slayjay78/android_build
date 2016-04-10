6f88bfa4d1da491232fa512226a7033cb7a873c4
frap129 <joe@frap129.org>
2016-04-09 19:56:41 -0500

    DragonTC: Update polly flags and fix krait

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -9,15 +9,8 @@ CLANG_CONFIG_arm_EXTRA_CFLAGS := \
 CLANG_CONFIG_arm_EXTRA_CPPFLAGS := \
   -no-integrated-as
 
-ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-  # Android's clang support's krait as a CPU whereas GCC doesn't. Specify
-  # -mcpu here rather than the more normal core/combo/arch/arm/armv7-a-neon.mk.
-  ifneq ($(TARGET_DRAGONTC_VERSION),)
-    CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=krait2 -mfpu=neon-vfpv4
-  else
-    CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=krait
-  endif
-endif
+CLANG_CONFIG_arm_EXTRA_CPPFLAGS := \
+  -no-integrated-as
 
 CLANG_CONFIG_arm_EXTRA_LDFLAGS := \
   -no-integrated-as
