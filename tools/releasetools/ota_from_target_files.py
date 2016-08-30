1609e46b354e2f94a1d4c220caca452336f732e4
Rusty <Is@High.com>
2016-08-29 20:24:37 -0400

    Revert "Show UBER in Recovery"
    This reverts commit 8518e14153130f65921c2558af1e0989ec3b855e.

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -610,18 +610,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
   # Dump fingerprints
-  #script.Print("Target: %s" % CalculateFingerprint(
-  #    oem_props, oem_dict, OPTIONS.info_dict))
-  script.Print("******************************************");
-  script.Print("*                                        *");
-  script.Print("* _|    _|  _|_|_|    _|_|_|_|  _|_|_|   *");
-  script.Print("* _|    _|  _|    _|  _|        _|    _| *");
-  script.Print("* _|    _|  _|_|_|    _|_|_|    _|_|_|   *");
-  script.Print("* _|    _|  _|    _|  _|        _|    _| *");
-  script.Print("* _|    _|  _|    _|  _|        _|    _| *");
-  script.Print("*   _|_|    _|_|_|    _|_|_|_|  _|    _| *");
-  script.Print("*                                        *");
-  script.Print("******************************************"); 
+  script.Print("Target: %s" % CalculateFingerprint(
+      oem_props, oem_dict, OPTIONS.info_dict))
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
